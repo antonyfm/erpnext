20cb4d9b1a314824c24ef167311fed60dfce48a8
Antony F. M <antonyfm@gmail.com>
2021-08-16 12:13:00 +0000

    quality_wps_v1

---
+++ erpnext/quality_management/doctype/procedure_qualification_record_pqr/procedure_qualification_record_pqr.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Procedure Qualification Record PQR', {
+	// refresh: function(frm) {
+
+	// }
+});
